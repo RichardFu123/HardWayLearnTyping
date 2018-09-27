b3c5a4a3f49915ab3ed53500a65a1e8eda25a7f0
Shawn <121857051@qq.com>
2018-09-27 10:00:58 +0800

    update : ruby : add code ex37~ex39

---
+++ Ruby/ex39.rb
@@ -0,0 +1,61 @@
+# create a mapping of state to abbreviation
+states = {
+  'Oregon' => 'OR',
+  'Florida' => 'FL',
+  'California' => 'CA',
+  'New York' => 'NY',
+  'Michigan' => 'MI'
+}
+
+# create a basic set of states and some cities in them
+cities = {
+  'CA' => 'San Francisco',
+  'MI' => 'Detroit',
+  'FL' => 'Jacksonville'
+}
+
+# add some more cities
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+# puts out some cities
+puts '-' * 10
+puts "Michigan`s abbreviation is: #{states['Michigan']}"
+puts "Florida`s abbreviation is #{states['Florida']}"
+
+# do it by using the state then cities dict
+puts '-' * 10
+puts "Michigan has: #{cities[states['Michigan']]}"
+puts "Florida has: #{cities[states['Florida']]}"
+
+# puts every state abbreviation
+puts '-' * 10
+states.each do |state, abbrev|
+  puts "#{state} is abbreviated #{abbrev}"
+end
+
+# puts every city in state
+puts '-' * 10
+cities.each do |abbrev,city|
+  puts "#{abbrev} has the city #{city}"
+end
+
+# now do both at the same time
+puts '-' * 10
+states.each do |state, abbrev|
+  city = cities[abbrev]
+  puts "#{state} is abbreviated #{abbrev} and has city #{city}"
+end
+
+puts '-' * 10
+# by default ruby says "nil" when something isn`t in there
+state = states['Texas']
+
+if !state
+  puts "Sorry, no Texas."
+end
+
+# default values using ||= with the nil result
+city = cities['TX']
+city ||= 'Does Not Exist'
+puts "The city for the state 'TX' is: #{city}"
